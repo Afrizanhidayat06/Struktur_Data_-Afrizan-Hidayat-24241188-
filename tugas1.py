967b851b547afac194b05739d3e929212edfb72f
Afrizan Hidayat /Alprog-e <afrizanhidayat06@gmail.com>
2025-04-15 07:20:11 +0800

    Add files via upload

---
+++ tugas1.py
@@ -0,0 +1,22 @@
+x = 24
+y= 85
+hasil = x < y
+print(hasil)
+
+
+hasil = x > y
+print(hasil)
+
+
+
+hasil = x <= y
+print(hasil)
+
+hasil = x >= y
+print(hasil)
+
+hasil = x == y
+print(hasil)
+
+hasil = x != y
+print(hasil)
